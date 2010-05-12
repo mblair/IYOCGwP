6ad5c21a1ec7772f2262716f2a447d196bdfb263
Matt Blair <me@matthewblair.net>
2010-05-11 22:55:21 -0400

    hello :-)

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# This program says hello and asks for my name.
+# Matt Blair <me@matthewblair.net>
+# 5-11-10 22:17 EST
+
+print('Hello world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName + '!')
