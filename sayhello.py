95b7a7dc3b4b7b1e047ad014a0d666c4a420b201
Matt Blair <me@matthewblair.net>
2010-05-18 18:20:17 -0400

    chapter 6 tangent

---
+++ sayhello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Chapter 6: parameter explanation.
+
+def sayHello(name):
+    print('Hello ' + name)
+
+print('Say hello to Alice.')
+fizzy = 'Alice'
+sayHello(fizzy)
+print('Do not forget to say hello to Bob.')
+sayHello('Bob')
