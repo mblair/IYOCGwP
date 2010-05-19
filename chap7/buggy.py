5f28e2c0af629f1018bff57192253ace96bee116
Matt Blair <me@matthewblair.net>
2010-05-19 01:57:57 -0400

    learning pdb

---
+++ chap7/buggy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Chapter 7: Buggy.
+
+import random
+number1 = random.randint(1,10)
+number2 = random.randint(1,10)
+print('What is ' + str(number1) + ' + ' + str(number2) + '?')
+answer = input()
+if answer == number1 + number2:
+    print('Correct!')
+else:
+    print('Nope! The answer is ' + str(number1 + number2))
